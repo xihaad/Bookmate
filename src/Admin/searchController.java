ff4d36fb5f55302f066acc4f04cfaa7b7f6f77f9
xihaad <zihad.rafi16@gmail.com>
2022-08-22 02:24:13 +0600

    search page designed

--- src/Admin/searchController.java
+++ src/Admin/searchController.java
@@ -7,25 +7,28 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class homeController {
-    public Admin.SqliteQueries sqlitequeries=new Admin.SqliteQueries();
-
-
+public class searchController {
     @FXML
-    private Button searchbtn;
+    private Button homebtn;
 
-    public void SearchCheck(ActionEvent e) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("search.fxml"));
+
+
+
+
+
+
+
+    public void goTohome(ActionEvent e) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("home.fxml"));
         Stage stage =  (Stage) ((Node) e.getSource()).getScene().getWindow();
         stage.setTitle("Bookmate");
         stage.setScene(new Scene(root,640,430));
         stage.show();
     }
+
+
 }
